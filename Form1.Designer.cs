02f9078a989d60223fca0065eccd191f0f7dc5b2
Famiglia Contadini <famcontadini@outlook.it>
2021-04-12 15:56:49 +0200

    Aggiungere i file di progetto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace scacchieragioco
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.lbl_pc = new System.Windows.Forms.Label();
+            this.lbl_gioc = new System.Windows.Forms.Label();
+            this.btn_turnosucc = new System.Windows.Forms.Button();
+            this.btn_inizia = new System.Windows.Forms.Button();
+            this.tab_s = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.tab_s)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_pc
+            // 
+            this.lbl_pc.AutoSize = true;
+            this.lbl_pc.Font = new System.Drawing.Font("Microsoft YaHei UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_pc.Location = new System.Drawing.Point(274, 9);
+            this.lbl_pc.Name = "lbl_pc";
+            this.lbl_pc.Size = new System.Drawing.Size(56, 16);
+            this.lbl_pc.TabIndex = 9;
+            this.lbl_pc.Text = "Punti Pc:";
+            // 
+            // lbl_gioc
+            // 
+            this.lbl_gioc.AutoSize = true;
+            this.lbl_gioc.Font = new System.Drawing.Font("Microsoft YaHei UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_gioc.Location = new System.Drawing.Point(133, 9);
+            this.lbl_gioc.Name = "lbl_gioc";
+            this.lbl_gioc.Size = new System.Drawing.Size(96, 16);
+            this.lbl_gioc.TabIndex = 8;
+            this.lbl_gioc.Text = "Punti Giocatore:";
+            // 
+            // btn_turnosucc
+            // 
+            this.btn_turnosucc.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_turnosucc.Location = new System.Drawing.Point(12, 264);
+            this.btn_turnosucc.Name = "btn_turnosucc";
+            this.btn_turnosucc.Size = new System.Drawing.Size(80, 23);
+            this.btn_turnosucc.TabIndex = 7;
+            this.btn_turnosucc.Text = "Avanza";
+            this.btn_turnosucc.UseVisualStyleBackColor = true;
+            this.btn_turnosucc.Visible = false;
+            this.btn_turnosucc.Click += new System.EventHandler(this.btn_turnosucc_Click);
+            // 
+            // btn_inizia
+            // 
+            this.btn_inizia.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_inizia.Location = new System.Drawing.Point(12, 264);
+            this.btn_inizia.Name = "btn_inizia";
+            this.btn_inizia.Size = new System.Drawing.Size(80, 23);
+            this.btn_inizia.TabIndex = 6;
+            this.btn_inizia.Text = "Inizia";
+            this.btn_inizia.UseVisualStyleBackColor = true;
+            this.btn_inizia.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tab_s
+            // 
+            this.tab_s.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.22F);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.Red;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.tab_s.DefaultCellStyle = dataGridViewCellStyle3;
+            this.tab_s.Location = new System.Drawing.Point(136, 38);
+            this.tab_s.Name = "tab_s";
+            this.tab_s.Size = new System.Drawing.Size(259, 249);
+            this.tab_s.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(401, 302);
+            this.Controls.Add(this.lbl_pc);
+            this.Controls.Add(this.lbl_gioc);
+            this.Controls.Add(this.btn_turnosucc);
+            this.Controls.Add(this.btn_inizia);
+            this.Controls.Add(this.tab_s);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximumSize = new System.Drawing.Size(421, 345);
+            this.MinimumSize = new System.Drawing.Size(421, 345);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Gioco della Scacchiera";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tab_s)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_pc;
+        private System.Windows.Forms.Label lbl_gioc;
+        private System.Windows.Forms.Button btn_turnosucc;
+        private System.Windows.Forms.Button btn_inizia;
+        private System.Windows.Forms.DataGridView tab_s;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace scacchieragioco
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rnd = new Random();
+        int rr = 0, cc = 0;
+        int rrr = 0, ccc = 0;
+        int pntut = 0, pntpc = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tab_s.Rows[0].Cells[0].Selected = false; //tolgo l'evidenziatura di defualt
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            btn_turnosucc.Visible = true;
+            btn_inizia.Visible = false;
+            tab_s.RowCount = 6;
+            tab_s.ColumnCount = 6;
+
+            for (int r = 0; r < 6; r++)
+            {
+
+                for (int c = 0; c < 6; c++) //ridimensiono le celle
+                {
+                    tab_s.Rows[r].Height = 35;
+                    tab_s.Columns[c].Width = 35;
+
+                }
+            }
+
+            for (int r = 0; r < 6; r++)
+            {
+
+                for (int c = 0; c < 6; c++)
+                {
+
+                    if (r % 2 == 0) 
+                    {
+                        if (c % 2 == 0) //vadoa a fare la scacchiera
+                            tab_s.Rows[r].Cells[c].Style.BackColor = Color.Black;
+                        else
+                            tab_s.Rows[r].Cells[c].Style.BackColor = Color.White;
+                    }
+
+                    else 
+                    {
+
+                        if (c % 2 != 0)
+                            tab_s.Rows[r].Cells[c].Style.BackColor = Color.Black;
+                        else
+                            tab_s.Rows[r].Cells[c].Style.BackColor = Color.White;
+                    }
+
+                }
+            }
+        }
+        private void puliscitab()
+        {
+
+            for (int r = 0; r < 6; r++)
+            {
+
+                for (int c = 0; c < 6; c++)
+                { //pulisco la tab
+
+
+                    tab_s.Rows[r].Cells[c].Value = "";
+
+                }
+
+            }
+
+        }
+        private void btn_turnosucc_Click(object sender, EventArgs e)
+        {
+            puliscitab();
+
+            rr = rnd.Next(0, 6);
+            cc = rnd.Next(0, 6);
+
+            if (tab_s.Rows[rr].Cells[cc].Style.BackColor == Color.White) //se la X é presente in una cella nera la X é bianca
+                tab_s.Rows[rr].Cells[cc].Style.ForeColor = Color.Black;
+            else
+                tab_s.Rows[rr].Cells[cc].Style.ForeColor = Color.White; //viceversa l:98
+
+            tab_s.Rows[rr].Cells[cc].Value = "X";
+
+
+            do
+            {
+                rrr = rnd.Next(0, 6);
+                ccc = rnd.Next(0, 6);
+            }
+            while (rrr == rr && ccc == cc);
+
+            if (tab_s.Rows[rrr].Cells[ccc].Style.BackColor == Color.White)
+                tab_s.Rows[rrr].Cells[ccc].Style.ForeColor = Color.Black;
+            else
+                tab_s.Rows[rrr].Cells[ccc].Style.ForeColor = Color.White;
+            tab_s.Rows[rrr].Cells[ccc].Value = "X";
+
+            assegnpnt();
+        }
+
+        private void assegnpnt()
+        { //assegno i punti a utente e pc
+
+            if (tab_s.Rows[rr].Cells[cc].Style.BackColor == tab_s.Rows[rrr].Cells[ccc].Style.BackColor)
+                pntut += 1;
+
+            else
+                pntpc += 1;
+
+            lbl_gioc.Text = "Punti Giocatore: " + pntut;
+            lbl_pc.Text = "Punti Pc: " + pntpc;
+
+            //controllo se ha vinto l'utente 
+            if (pntut == 5)
+            {
+
+                MessageBox.Show("Ha vinto l'utente :)");               
+                btn_inizia.Visible = true;
+                btn_turnosucc.Visible = false;
+                puliscitab();
+
+                pntut = 0;
+                pntpc = 0;
+
+                lbl_gioc.Text = "Punti Giocatore:";
+                lbl_pc.Text = "Punti Pc:";
+                for (int r = 0; r < 6; r++)
+                {
+
+                    for (int c = 0; c < 6; c++)
+                    {
+
+
+                        tab_s.Rows[r].Cells[c].Style.BackColor = Color.White;
+
+                    }
+
+                }
+
+            }
+            // controllo se ha vinto il pc 
+            else if (pntpc == 5)
+            {
+
+                MessageBox.Show("Ha vinto il pc :(");              
+                btn_inizia.Visible = true;
+                btn_turnosucc.Visible = false;
+                puliscitab();
+
+                pntut = 0;
+                pntpc = 0;
+
+                lbl_gioc.Text = "Punti Giocatore:";
+                lbl_pc.Text = "Punti Pc:";
+                for (int r = 0; r < 6; r++)
+                {
+
+                    for (int c = 0; c < 6; c++)
+                    {
+
+
+                        tab_s.Rows[r].Cells[c].Style.BackColor = Color.White;
+
+                    }
+
+                }
+
+
+            }
+
+
+        }
+
+       
+    }
+   
+    
+}
